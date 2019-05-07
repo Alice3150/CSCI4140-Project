e1053ea86752470649075b8ca99ccf9382733491
SuWeixin <1155107966@link.cuhk.edu.hk>
2019-05-07 21:24:12 +0800

    Add signup/login

--- deployment/run_server.py
+++ deployment/run_server.py
@@ -1,7 +1,8 @@
 import tensorflow as tf
-from flask import Flask, render_template, request, url_for, redirect
+from flask import Flask, render_template, request, url_for, redirect, flash, make_response, session
 from wtforms import SubmitField
 import os
+from pymongo import MongoClient
 
 from flask_wtf import FlaskForm
 from flask_wtf.file import FileField, FileAllowed, FileRequired
@@ -33,6 +34,11 @@ scream_ckpt = PROJECT_PATH + 'model_styles/scream.ckpt'
 udnie_ckpt = PROJECT_PATH + 'model_styles/udnie.ckpt'
 wave_ckpt = PROJECT_PATH + 'model_styles/wave.ckpt'
 wreck_ckpt = PROJECT_PATH + 'model_styles/wreck.ckpt'
+
+# Connect to the database
+# Please specify your database
+client = MongoClient("mongodb+srv://csci4140:csci4140@esocluster-90004.mongodb.net/admin")
+db = client.projdb
 
 class ImageForm(FlaskForm):
     # input image
@@ -145,6 +151,13 @@ def home():
         wreck_file_url = '/static/images/' + filename
     else:
         wreck_file_url = None
+    
+    loginMessage = "Sign up / Log in"
+    loginUrl = "signup"
+
+    if 'username' in session:
+        loginUrl = "#"
+        loginMessage = session['username']
 
     return render_template('index.html', 
         screamForm=screamForm, scream_file_url=scream_file_url, 
@@ -152,15 +165,59 @@ def home():
         rainPrincessForm=rainPrincessForm, rainPrincess_file_url=rainPrincess_file_url,
         udnieForm=udnieForm, udnie_file_url=udnie_file_url,
         waveForm=waveForm, wave_file_url=wave_file_url,
-        wreckForm=wreckForm, wreck_file_url=wreck_file_url)
+        wreckForm=wreckForm, wreck_file_url=wreck_file_url,
+        loginMessage=loginMessage, loginUrl=loginUrl)
 
-@app.route('/signup')
+@app.route('/signup', methods=['POST', 'GET'])
 def signup(name=None):
-    return render_template('signup.html', name=name)
+    error = ""
+    count = 0
 
-@app.route('/login')
+    if request.method == 'POST':
+        _name = request.form['inputUsername']
+        _password = request.form['inputPassword']
+        _rpassword = request.form['repeatPassword']
+        users = db.Users.find({'name':_name})
+
+        for u in users:
+            count += 1
+
+        if count != 0:
+            error="Username already exists!"
+        elif _password != _rpassword:
+            error="password not coincident!"
+        else:
+            db.Users.insert({'name':_name, 'password':_password, 'images':[]})
+            flash('You were successfully signed up')
+            return redirect(url_for('login'))
+
+    return render_template('signup.html', name=name, error=error)
+
+@app.route('/login', methods=['POST', 'GET'])
 def login(name=None):
-    return render_template('login.html', name=name)
+    error = ""
+    count = 0
+
+    if request.method == 'POST':
+        _name = request.form['inputUsername']
+        _password = request.form['inputPassword']
+        users = db.Users.find({'name':_name}, {'password':_password})
+
+        for u in users:
+            count += 1
+
+        if count == 0:
+            error="Username or password not correct!"
+        else:
+            session['username'] = _name
+            return redirect(url_for('home'))
+
+    return render_template('login.html', name=name, error=error)
+
+@app.route('/logout')
+def logout():
+    session.pop('username', None)
+    return redirect(url_for('home'))
 
 if __name__ == "__main__":
     print(("* Loading model and Flask starting server..."
